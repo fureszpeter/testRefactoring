09b56f8aa2fe23b0f1c1e50c151e522dea1de0cc
Furesz Peter <fureszpeter@gmail.com>
2016-02-20 17:36:27 +0100

    Add Travis to run tests

---
+++ tests/InitialTest.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Class TestInitial.
+ *
+ * @license MIT
+ */
+class InitialTest extends PHPUnit_Framework_TestCase
+{
+    public function testTrueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
