eedef3ccb95e673ea9a1c58fd14b62f81d1e313b
Furesz Peter <fureszpeter@gmail.com>
2016-02-21 14:42:32 +0100

    [WIP] generate_language_files

--- src/generate_language_files.php
+++ src/generate_language_files.php
@@ -4,6 +4,7 @@ chdir(__DIR__);
 
 include '../vendor/autoload.php';
 
-$languageBatchBo = new \Language\LanguageBatchBo();
+//$languageBatchBo = new \Language\LanguageBatchBo();
+$languageBatchBo = new \Language\Infrastructure\Services\BashLanguageFileGenerator();
 $languageBatchBo->generateLanguageFiles();
 $languageBatchBo->generateAppletLanguageXmlFiles();
