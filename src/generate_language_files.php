3fcff01516656de4f0c2b00ab192396979a430f8
Furesz Peter <fureszpeter@gmail.com>
2016-02-23 00:22:14 +0100

    call the new version of code

--- src/generate_language_files.php
+++ src/generate_language_files.php
@@ -1,10 +1,12 @@
 <?php
 
+use Language\Domain\Factories\BashLanguageFileGeneratorFactory;
+
 chdir(__DIR__);
 
 include '../vendor/autoload.php';
 
 //$languageBatchBo = new \Language\LanguageBatchBo();
-$languageBatchBo = new \Language\Infrastructure\Services\BashLanguageFileGenerator();
+$languageBatchBo = BashLanguageFileGeneratorFactory::create();
 $languageBatchBo->generateLanguageFiles();
 $languageBatchBo->generateAppletLanguageXmlFiles();
